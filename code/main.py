703d347dd71413c527af72089726dfa5465912a6
TomasKulzer <up202207332@g.uporto.pt>
2025-11-09 15:30:39 +0000

    __pycache__

--- code/main.py
+++ code/main.py
@@ -1,7 +1,8 @@
-from image_loader import ImageLoader
+from data_loading_and_preprocessing.image_loader import ImageLoader
+
 
 if __name__ == "__main__":
-    loader = ImageLoader('path/to/training_data', image_size=(224,224))
+    loader = ImageLoader('../training_data', image_size=(224,224))
     print(f"Found classes: {loader.class_names}")
     print(f"Number of samples: {len(loader)}")
     image, label = loader[0]
